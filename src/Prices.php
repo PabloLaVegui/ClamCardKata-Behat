359d9e77ec9e97c6fae3a04f5cdf47849c79d7d1
PabloLavegui <pab.gutier@gmail.com>
2016-11-03 11:52:47 +0100

    feat(prices): calculate journey price in case of the journey is between different zones

--- src/Prices.php
+++ src/Prices.php
@@ -1,23 +1,28 @@
 <?php
 namespace ClamCardKata;
 
+use \ClamCardKata\Journey;
+
 class Prices
 {
     const PRICES = [
-        'zoneA' => 2.5
+        'zoneA' => 2.5,
+        'zoneB' => 3.0
     ];
 
-    public function getPrice($fromStation, $toStation)
+    public function getPrice(Journey $journey)
     {
-        $fromZone = Stations::getZone($fromStation);
-        $toZone   = Stations::getZone($toStation);
+        $fromZone = Stations::getZone($journey->from());
+        $toZone   = Stations::getZone($journey->to());
 
         return $this->getPriceByZones($fromZone, $toZone);
     }
 
     private function getPriceByZones($fromZone, $toZone)
     {
-        return $fromZone === $toZone ? $this->getZonePrice($fromZone) : 0;
+        return $fromZone === $toZone
+                    ? $this->getZonePrice($fromZone)
+                    : $this->getZonePrice('zoneB');
     }
 
     private function getZonePrice($zone)
